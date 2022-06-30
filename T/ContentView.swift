715afd8f589a7a0480475696b9af54481f726c45
User-Howard <howard20060216@gmail.com>
2022-06-30 15:41:52 +0800

    Initial Commit

---
+++ T/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  T
+//
+//  Created by 吳浩瑋 on 2022/6/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ T/TApp.swift
@@ -0,0 +1,17 @@
+//
+//  TApp.swift
+//  T
+//
+//  Created by 吳浩瑋 on 2022/6/30.
+//
+
+import SwiftUI
+
+@main
+struct TApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
