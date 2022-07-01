eb4beac4b568c97927681d6e3b723156ea0dd04d
User-Howard <howard20060216@gmail.com>
2022-07-01 23:27:41 +0800

    init

--- T/ContentView.swift
+++ T/ContentView.swift
@@ -7,10 +7,85 @@
 
 import SwiftUI
 
+
+struct MemberView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.blue
+            }.navigationTitle(NSLocalizedString("Member", comment: ""))
+        }
+    }
+}
+struct TaskView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.blue
+            }.navigationTitle(NSLocalizedString("Task", comment: ""))
+        }
+    }
+}
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.gray
+                VStack{
+                    Image(systemName: "person")
+                    Image(systemName: "person.fill")
+                }
+            }.navigationTitle(NSLocalizedString("Home", comment: ""))
+        }
+    }
+}
+struct ShopView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.blue
+            }.navigationTitle(NSLocalizedString("Shop", comment: ""))
+        }
+    }
+}
+struct RankView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Color.blue
+            }.navigationTitle(NSLocalizedString("Rank", comment: ""))
+        }
+    }
+}
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            MemberView()
+                .tabItem{
+                    Image(systemName: "person")
+                    Text(NSLocalizedString("Member", comment: ""))
+                }
+            TaskView()
+                .tabItem{
+                    Image(systemName: "star")
+                    Text(NSLocalizedString("Task", comment: ""))
+                }
+            HomeView()
+                .tabItem{
+                    Image(systemName: "house")
+                    Text(NSLocalizedString("Home", comment: ""))
+                }
+            ShopView()
+                .tabItem{
+                    Image(systemName: "bag")
+                    Text(NSLocalizedString("Shop", comment: ""))
+                }
+            RankView()
+                .tabItem{
+                    Image(systemName: "rosette")
+                    Text(NSLocalizedString("Rank", comment: ""))
+                }
+        }
     }
 }
 
